03aec2e554af86c32582d39416de26a0109df990
Nayar Joolfoo <nayar@joolfoo.com>
2025-09-16 20:10:40 +0400

    add classes

---
+++ week18/d1/myclasses.js
@@ -0,0 +1,9 @@
+class BankAccount {
+    constructor(first_name, last_name, type_of_account) {
+        this.first_name = first_name
+        this.last_name = last_name
+        this.type_of_account = type_of_account
+    }
+}
+
+yajnee = new BankAccount("Yajnee","Ahota", 'Savings')
