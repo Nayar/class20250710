5baa3e04385cf553a376a631faae48658e27cf26
Nayar Joolfoo <nayar@joolfoo.com>
2025-09-16 20:30:09 +0400

    bank class

--- week18/d1/myclasses.js
+++ week18/d1/myclasses.js
@@ -3,7 +3,28 @@ class BankAccount {
         this.first_name = first_name
         this.last_name = last_name
         this.type_of_account = type_of_account
+        this.balance = 0 // this should be private
+    }
+
+    deposit(amount){ 
+        this.balance = this.balance + amount
+    }
+
+    withdraw(amount){
+        if(amount <= this.balance){
+            this.balance = this.balance - amount
+        }
+        else {
+            console.log("insufficient amount to withdraw")
+        }
+    }
+
+    checkBalance(){
+        console.log('Hello ' +  this.first_name + ". You currently have " + this.balance)
     }
 }
 
 yajnee = new BankAccount("Yajnee","Ahota", 'Savings')
+yajnee.deposit(2500)
+yajnee.withdraw(1000)
+yajnee.checkBalance()
