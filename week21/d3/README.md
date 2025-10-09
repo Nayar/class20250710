a6b5aa22b2a9e45c1dbd4cdcb512d3aaf33c0c52
Nayar Joolfoo <nayar@joolfoo.com>
2025-10-09 21:03:58 +0400

    knex class

---
+++ week21/d3/README.md
@@ -0,0 +1,7 @@
+Steps
+=====
+npm init
+npm install knex
+npm install pg // this is postgres
+npm install express
+touch index.js
---
+++ week21/d3/index.js
@@ -0,0 +1,75 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json());
+
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1', // localhost
+        user: 'postgres',
+        password: '',
+        database: 'hellokitty',
+        port: 5432
+    }
+});
+
+app.get("/api/students",function(request, response){
+    // select name, nic from students;
+    knex.select('*').from('students')
+    .then(data => {
+        response.json(data)})
+})
+
+app.get("/api/students/:id",function(request, response){
+    // select name, nic from students;
+    const id = request.params.id
+
+    knex.select('*').from('students').where({ id: id})
+    .then(data => {
+        response.json(data)})
+})
+
+app.post("/api/students", function(request,response){
+    console.log("receiving student to create")
+    console.log(request.body)
+    console.log(request.body.name)
+    // INSERT INTO students (name) VALUES ("Sarah") RETURNING name,id,nic
+    knex('students').returning(['name', 'id', 'nic']).insert(
+        {
+            name:request.body.name,
+            nic: request.body.nic
+        }
+    ).then(data => {console.log(data)
+        response.json({'status':"ok","data":data})
+    })
+})
+
+app.put("/api/students/:id",(req,res) => {
+    
+    const student_id = req.params.id
+    const new_data = req.body
+
+    console.log(student_id,new_data)
+
+    // UPDATE students SET name = "Ritesh Smith" WHERE id = {id}
+    knex('students').where({id:9}).update(req.body).returning('*').then(function(data){
+        console.log(data)
+        res.json({'status':"ok","data":data})
+    })
+})
+
+app.delete("/api/students/:id", (req,res) => {
+    const student_id = req.params.id
+    // DELETE FROM students WHERE id={id}
+    knex('students').where({id:student_id}).del().then(function(data) {
+        res.json({'status':"ok"})
+    })
+})
+
+
+
+// go to localhost:5005/api/students
+app.listen(5005, function(){
+    console.log("server is running")
+})
