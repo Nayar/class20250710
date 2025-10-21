506a622fc8c62fbc449ea8a0428f070eb04de8c4
Nayar Joolfoo <nayar@joolfoo.com>
2025-10-21 18:59:16 +0400

    mycarbusiness

--- week23/d1/mycarbusiness/src/index.js
+++ week23/d1/mycarbusiness/src/index.js
@@ -3,18 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Car from './Car';
-import Sunrise from './Sunrise';
-import SunriseFunction from './SunriseFunction';
-import CarFunction from './CarFunction';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Car make="BMW" model="x1" max_speed="200"/>
-    <Car make="VW" model="polo" max_speed="150"/>
-    <Car make="Nissan" model="March" max_speed="110"/>
-    <CarFunction max_speed="110"/>
-    <CarFunction max_speed="200"/>
+    <Car/>
+    <Car/>
+    <Car/>
   </React.StrictMode>
 );
 
@@ -22,8 +17,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-// 0km/h -> 2s
-// 100km/h -> 0.5s
-
